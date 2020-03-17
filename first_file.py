e1ea43b7ad8fcb01a7e70aaa2f2c3e8cf8832367
Dennis Hefke <dennis.hefke@outlook.com>
2020-03-17 10:16:01 +0100

    added first map files

--- first_file.py
+++ first_file.py
@@ -4,4 +4,40 @@ Spyder Editor
 
 This is a temporary script file.
 """
-print('Hello world')
+
+import pygame
+import pytmx
+
+pygame.init()
+screen = pygame.display.set_mode((240, 256))
+done = False
+is_blue = True
+x = 30
+y = 30
+clock = pygame.time.Clock()
+gameMap = pytmx.load_pygame('grass_map.tmx')
+
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            is_blue = not is_blue
+    
+    pressed = pygame.key.get_pressed()
+    if pressed[pygame.K_UP]: y -= 3
+    if pressed[pygame.K_DOWN]: y += 3
+    if pressed[pygame.K_LEFT]: x -= 3
+    if pressed[pygame.K_RIGHT]: x += 3
+    
+    screen.fill((0,0,0))
+    
+    if is_blue: color = (0, 128, 255)
+    else: color = (255, 100, 0)
+    pygame.draw.rect(screen, color, pygame.Rect(x, y, 60, 60))
+    
+    clock.tick(60)
+    pygame.display.update()
+
+pygame.quit()
